e74847c8dd6fc616a7027b94dc9832faa279b1e2
TecharyJuliana <107491117+TecharyJuliana@users.noreply.github.com>
2024-08-13 10:30:37 +0100

    Add files via upload

---
+++ GetSharedMailboxPermissions.ps1
@@ -0,0 +1,29 @@
+﻿Connect-ExchangeOnline
+# List of shared mailboxes
+$sharedMailboxes = @(
+  
+    # Add more shared mailboxes as needed
+)
+
+# Path to save the CSV file eg * change location to export file 
+$outputFile = "/Users/jlucena/Documents/AllSharedMailboxMembers.csv"
+
+# Initialize an empty array to store results
+$allResults = @()
+
+# Loop through each shared mailbox
+foreach ($sharedMailbox in $sharedMailboxes) {
+    $members = Get-MailboxPermission -Identity $sharedMailbox | Where-Object { $_.AccessRights -eq "FullAccess" -and $_.User -notlike "NT AUTHORITY\SELF" } | Select-Object User
+
+    foreach ($member in $members) {
+        $userDetails = Get-Recipient -Identity $member.User
+        $allResults += [PSCustomObject]@{
+            SharedMailbox = $sharedMailbox
+            Name          = $userDetails.DisplayName
+            Email         = $userDetails.PrimarySmtpAddress
+        }
+    }
+}
+
+# Export the combined results to a CSV file
+$allResults | Export-Csv -Path $outputFile -NoTypeInformation
